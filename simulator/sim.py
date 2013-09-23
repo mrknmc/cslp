45a3c2619e52c23cde7af7a99a02af29ce6e0db8
Mark Nemec <mrknmc@me.com>
2013-09-23 18:44:11 +0100

    renamed simulator and moved the tests

--- simulator/sim.py
+++ simulator/sim.py
@@ -76,11 +76,11 @@ class Simulator:
         Run the simulation until world explodes.
         """
         while self.time <= self.max_time:
-            pass  # From the current state calculate the set of events which may occur total rate ← the sum of the rates of those events
-            pass  # delay ← choose a delay based on the total rate
-            pass  # event ← choose probabilistically from those events
+            pass  # From the current state calculate the set of events which may occur total rate <- the sum of the rates of those events
+            pass  # delay <- choose a delay based on the total rate
+            pass  # event <- choose probabilistically from those events
             pass  # modify the state of the system based on the chosen event
-            pass  # time ← time + delay
+            pass  # time <- time + delay
 
 
 if __name__ == '__main__':
