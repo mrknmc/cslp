08dfd7d4e0a2ef609de8a1fae8b41f63738b9eef
Mark Nemec <mrknmc@me.com>
2013-09-23 18:45:48 +0100

    wrote unit tests for models

---
+++ tests/models_tests.py
@@ -0,0 +1,101 @@
+import unittest
+
+from simulator.models import Network, Route, Bus, Passenger
+
+
+class TestNetwork(unittest.TestCase):
+
+    def setUp(self):
+        self.network = Network()
+
+
+class TestBus(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_id(self):
+        """
+        """
+        bus = Bus(12, 4, 20, 4)
+        self.assertEqual(bus.uid, '12.4')
+
+    def test_ready_for_departure(self):
+        """
+
+        """
+        buses = (
+            Bus(1, 0, 20, 2),
+            Bus(1, 0, 20, 2),
+        )
+        # TODO: finish this
+
+    def test_disembarking_passengers(self):
+        """
+
+        """
+        bus1 = Bus(12, 4, 20, 5)
+        bus2 = Bus(12, 4, 20, 2)
+
+        passengers = [
+            Passenger(4, 5),
+            Passenger(2, 7),
+            Passenger(4, 5),
+            Passenger(4, 9),
+            Passenger(6, 1),
+        ]
+
+        bus1.passengers = passengers
+        bus2.passengers = passengers
+
+        exit_pax = bus1.disembarking_passengers()
+
+        self.assertTrue(bus1.passengers[0] in exit_pax)
+        self.assertTrue(bus1.passengers[2] in exit_pax)
+        self.assertEqual(len(exit_pax), 2)
+
+        exit_pax = bus2.disembarking_passengers()
+        self.assertEqual(exit_pax, [])
+
+    def test_full_capacity(self):
+        bus = Bus(12, 4, 20, 5)
+
+        bus.passengers = [Passenger(4, 5)] * 19
+        self.assertFalse(bus.full_capacity())
+
+        bus.board_passenger(Passenger(4, 5))
+        self.assertTrue(bus.full_capacity())
+
+        bus.board_passenger(Passenger(4, 5))
+        self.assertRaises(Exception, bus.full_capacity)
+
+    def test_no_disembarks(self):
+        bus1 = Bus(12, 4, 20, 5)
+        bus2 = Bus(12, 4, 20, 2)
+
+        passengers = [
+            Passenger(4, 5),
+            Passenger(2, 7),
+            Passenger(4, 5),
+            Passenger(4, 9),
+            Passenger(6, 1),
+        ]
+
+        bus1.passengers = passengers
+        bus2.passengers = passengers
+
+        disembarks = bus1.no_disembarks()
+        self.assertEqual(disembarks, False)
+
+        disembarks = bus2.no_disembarks()
+        self.assertEqual(disembarks, True)
+
+
+class TestStop(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
