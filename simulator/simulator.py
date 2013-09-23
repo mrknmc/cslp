6275dbd193983b2c3b2a15a7f2f8410eb407c5f0
Mark Nemec <mrknmc@me.com>
2013-09-23 13:43:28 +0100

    added the main simulator file

---
+++ simulator/simulator.py
@@ -0,0 +1,19 @@
+
+
+class Simulator:
+
+    def __init__(self, max_time):
+        self.max_time = max_time
+        self.time = 0.0
+
+    def run():
+        while self.time <= self.max_time:
+            pass  # From the current state calculate the set of events which may occur total rate ← the sum of the rates of those events
+            pass  # delay ← choose a delay based on the total rate
+            pass  # event ← choose probabilistically from those events
+            pass  # modify the state of the system based on the chosen event
+            pass  # time ← time + delay
+
+
+if __name__ == '__main__':
+    main()
