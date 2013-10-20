90e1aa18e73d5096e4fad2effcb3a077dcd6b4f8
Mark Nemec <mrknmc@me.com>
2013-10-20 17:39:39 +0100

    parser is now a set of functions instead of a class

--- simulator/parser.py
+++ simulator/parser.py
@@ -4,82 +4,95 @@ from simulator.models import Network
 
 INT_RX = r'\d+'
 FLOAT_RX = r'(\d+(\.\d*)?|\.\d+)'
+NEWLINE_COMMENT_RX = r'^(\n|#.*\n)$'  # matches newline or a comment
 
 ROUTE_RX = r'^route (?P<route_id>{0}) stops (?P<stop_ids>{0}( {0})*) buses (?P<bus_count>{0}) capacity (?P<bus_capacity>{0})$'.format(INT_RX)
 ROAD_RX = r'^road (?P<origin>{0}) (?P<destination>{0}) (?P<rate>{1})$'.format(INT_RX, FLOAT_RX)
-BOARDS_RATE_RX = r'^board ({0})$'.format(FLOAT_RX)
-DISEMBARKS_RATE_RX = r'^disembarks ({0})$'.format(FLOAT_RX)
-DEPARTS_RATE_RX = r'^departs ({0})$'.format(FLOAT_RX)
-NEW_PASSENGERS_RATE_RX = r'^new passengers ({0})$'.format(FLOAT_RX)
-STOP_TIME_RX = r'^stop time ({0})$'.format(FLOAT_RX)
+RATES_RX = (
+    r'^board (?P<board>{0})$'.format(FLOAT_RX),
+    r'^disembarks (?P<disembarks>{0})$'.format(FLOAT_RX),
+    r'^departs (?P<departs>{0})$'.format(FLOAT_RX),
+    r'^new passengers (?P<new_passengers>{0})$'.format(FLOAT_RX),
+    r'^stop time (?P<stop_time>{0})$'.format(FLOAT_RX),
+)
 IGNORE_WARN_RX = r'^ignore warnings$'
 OPTIMIZE_RX = r'^optimise parameters$'
-NEWLINE_RX = r'^\n$'
-COMMENT_RX = r'^#.*\n$'
+
+ROUTE_TYPES = {
+    'route_id': int,
+    'stop_ids': lambda a: map(int, str.split(a, ' ')),  # '1 2 3' -> [1, 2, 3]
+    'bus_count': int,
+    'bus_capacity': int
+}
+
+ROAD_TYPES = {
+    'origin': int,
+    'destination': int,
+    'rate': float
+}
 
 
-class InputParser:
+def parse_file(filename):
     """
-    Object that is responsible for creating the network from the input file.
     """
+    network = Network()
+    rates = {}
+    flags = {}
+    with open(filename, 'r') as f:
+        for line_no, line in enumerate(f, start=1):
+            line = line.rstrip('\n')  # get rid of newline
 
-    def __init__(self, filename=None):
-        self.filename = filename
-        self.network = Network()
+            match = rxmatch(ROUTE_RX, line, ftype=ROUTE_TYPES)
+            if match:
+                network.add_route(**match)
+                continue
 
-    def parse_file(self):
-        if self.filename is None:
-            raise Exception("No input file specified!")
-        with open(self.filename) as f:
-            line_no = 1
-            for line in f:
-                self.parse_line(line.rstrip('\n'))  # get rid of newline
-                line_no += 1
-        # TODO:Finished parsing - do stuff like validate
-        return self.network
+            match = rxmatch(ROAD_RX, line, ftype=ROAD_TYPES)
+            if match:
+                network.add_road(**match)
+                continue
 
-    def parse_line(self, line):
-        """
-        Function that parses the line and calls an appropriate method
-        """
-        if self.rxmatch(ROUTE_RX, line):
-            self.network.add_route(**self.line_params)
-        elif self.rxmatch(ROAD_RX, line):
-            self.network.add_road(**self.line_params)
-        elif self.rxmatch(BOARDS_RATE_RX, line):
-            # SET BOARDS RATE
-            pass
-        elif self.rxmatch(DISEMBARKS_RATE_RX, line):
-            # SET DISEMBARKS RATE
-            pass
-        elif self.rxmatch(DEPARTS_RATE_RX, line):
-            # SET DEPARTS RATE
-            pass
-        elif self.rxmatch(NEW_PASSENGERS_RATE_RX, line):
-            # SET NEW PASSENGERS RATE
-            pass
-        elif self.rxmatch(STOP_TIME_RX, line):
-            # SET STOP TIME
-            pass
-        elif self.rxmatch(IGNORE_WARN_RX, line):
-            # SET IGNORE WARNINGS
-            pass
-        elif self.rxmatch(OPTIMIZE_RX, line):
-            # SET OPTIMIZE
-            pass
-        elif self.rxmatch(NEWLINE_RX, line) or self.rxmatch(COMMENT_RX, line):
-            return  # ignore empty lines and comments
+            for rate_rx in RATES_RX:
+                match = rxmatch(rate_rx, line, ftype=float)
+                if match:
+                    rates.update(**match)
+                    continue
 
-        # raise Exception('Invalid input on line {} of file {}'.format(line_no, self.line_no))
+            if rxmatch(IGNORE_WARN_RX, line):
+                flags['ignore_warn'] = True
+                continue
 
-    def rxmatch(self, pattern, string):
-        """
-        Helper function for matching regular expressions.
-        """
-        match = re.match(pattern, string)
-        if match:
-            self.line_params = match.groupdict()
-            if not self.line_params:
-                self.line_params = match.group()
-        return match
+            if rxmatch(OPTIMIZE_RX, line):
+                flags['optimize'] = True
+                continue
 
+            if rxmatch(NEWLINE_COMMENT_RX, line):
+                continue  # ignore empty lines and comments
+
+            raise Exception('Invalid input on line {0} of file {1}'.format(line_no, filename))
+
+    return network, rates, flags
+
+
+def rxmatch(pattern, string, ftype=None):
+    """
+    Helper function for matching regular expressions. Will convert matched
+    groups into correct type based on the ftype parameter.
+
+    Returns a dictionary if there are any params, True if not and False if
+    there is no match.
+    """
+    match = re.match(pattern, string)
+    if not match:
+        return False
+    groupdict = match.groupdict()
+    if not groupdict:
+        return True
+    if ftype:
+        if isinstance(ftype, dict):
+            for key, val in groupdict.iteritems():
+                groupdict[key] = ftype[key](val)
+        else:
+            for key, val in groupdict.iteritems():
+                groupdict[key] = ftype(val)
+    return groupdict
