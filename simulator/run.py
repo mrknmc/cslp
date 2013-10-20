35753d2cd27a185e751572e21d9e24e149f71284
Mark Nemec <mrknmc@me.com>
2013-10-20 17:48:09 +0100

    merged flags and rates into params variable

--- simulator/run.py
+++ simulator/run.py
@@ -1,8 +1,8 @@
+from world import World
+
+
 def main():
     world = World('/Users/mark/Projects/simulator/simulator/test')
-    return sim.network
-    # print(sim.network)
-    # sim.validate()
 
 
 if __name__ == '__main__':
