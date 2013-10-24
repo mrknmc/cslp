bce0a5d6ef6681768f295cfa54d50dd934614251
Mark Nemec <mrknmc@me.com>
2013-10-24 03:30:39 +0100

    added options for input and output files

--- simulator/run.py
+++ simulator/run.py
@@ -1,10 +1,18 @@
 from world import World
+import os
+import sys
 
 
-def main():
-    world = World('/Users/mark/Projects/simulator/tests/test')
+def main(argv):
+    cwd = os.getcwd()
+    input_f = os.path.join(cwd, argv[0] if argv else 'tests/test1')
+    if len(argv) > 1:
+        output_f = os.path.join(cwd, argv[1])
+        sys.stdout = open(output_f, 'w')
+
+    world = World(input_f)
     world.start()
 
 
 if __name__ == '__main__':
-    net = main()
+    net = main(sys.argv[1:])
