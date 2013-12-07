8cf3f1bd9c7ce7e02584a073951aa0573a9beb98
Mark Nemec <mrknmc@me.com>
2013-12-07 19:18:59 +0000

    Refactoring - turned disembarks into a list

--- simulator/events.py
+++ simulator/events.py
@@ -19,22 +19,14 @@ class EventMap(object):
     Dictionary-like data structure to hold all possible events.
     """
 
-    def __init__(self, network=None):
-        self.boards = {}
-        self.disembarks = {}
+    def __init__(self):
+        self.boards = defaultdict(lambda: Counter())
+        self.disembarks = []
         self.departs = []
         self.arrivals = []
-        # Initialise the network
-        if network:
-            for stop in network.stops.itervalues():
-                self.departs.extend(stop.bus_queue)
 
     def gen_boards(self):
         for bus, dct in self.boards.iteritems():
             for dest, count in dct.iteritems():
                 yield bus, dest, count
 
-    def gen_disembarks(self):
-        for bus, dct in self.disembarks.iteritems():
-            for dest, count in dct.iteritems():
-                yield bus, dest, count
