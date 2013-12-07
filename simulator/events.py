b09f3184889c340e50ed73851c8c2352bc1b9ef0
Mark Nemec <mrknmc@me.com>
2013-12-07 19:17:55 +0000

    Refactoring - updating the event messages for the event map

--- simulator/events.py
+++ simulator/events.py
@@ -1,10 +1,14 @@
+from collections import defaultdict, Counter
+from functools import partial
+from termcolor import colored
+
 
 def log(event_type, **kwargs):
     msgs = {
         'arrivals': 'Bus {bus} arrives at stop {bus.stop} at time {time}',
-        'departs': 'Bus {bus} leaves stop {bus.stop} at time {time}',
-        'boards': 'Passenger boards bus {bus} at stop {stop} with destination {dest} at time {time}',
-        'disembarks': 'Passenger disembarks bus {bus} at stop {stop} at time {time}',
+        'departs': 'Bus {bus} leaves stop {dest} at time {time}',
+        'boards': 'Passenger boards bus {bus} at stop {bus.stop} with destination {dest} at time {time}',
+        'disembarks': 'Passenger disembarks bus {bus} at stop {bus.stop} at time {time}',
         'new_passengers': 'A new passenger enters at stop {orig} with destination {dest} at time {time}'
     }
     print(msgs[event_type].format(**kwargs))
