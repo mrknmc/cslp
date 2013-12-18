b56056dbb111c89ca3fe8db0fd6435e7a66a86e8
Mark Nemec <mrknmc@me.com>
2013-12-18 17:49:31 +0100

    added more validation tests

--- tests/validation_tests.py
+++ tests/validation_tests.py
@@ -61,9 +61,13 @@ class TestNetworkValidation(unittest.TestCase):
         """Verifies that when we have a road rate for two stops but no
         route for them we have a warning."""
         input_str = (
-            'route 1 stops 1 2 buses 3 capacity 10',
+            'route 1 stops 1 2 5 buses 3 capacity 10',
+            'route 1 stops 1 4 buses 3 capacity 10',
             'road 1 2 0.3',
+            'road 1 4 0.3',
+            'road 4 1 0.3',
             'road 2 1 0.5',
+            'road 2 5 0.8',
             'road 4 5 0.8',
             'board 0.5',
             'disembarks 0.6',
@@ -75,36 +79,42 @@ class TestNetworkValidation(unittest.TestCase):
         with self.assertRaises(InputWarning):
             network.validate(rates, params)
 
-    def test_rate_zero_is_error(self):
-        """Verifies that if a rate is 0 we raise an error."""
+    def test_road_rate_for_no_route_is_warn(self):
+        """Verifies that when we have a road rate for two stops but
+        at least one of them is not on any route we raise a warning."""
         input_str = (
             'route 1 stops 1 2 buses 3 capacity 10',
             'road 1 2 0.3',
-            'road 2 1 0.2',
-            'board 0.4',
-            'disembarks 0.5',
-            'departs 0',
-            'new passengers 3',
+            'road 2 1 0.5',
+            'road 4 2 0.8',
+            'board 0.5',
+            'disembarks 0.6',
+            'departs 0.5',
+            'new passengers 5',
+            'stop time 80'
+        )
+        network, rates, params, experiments = parse_lines(input_str, 'test')
+        with self.assertRaises(InputWarning):
+            network.validate(rates, params)
+
+    def test_route_with_same_stop_consecutively_is_error(self):
+        """Verifies that when we have a route with the same stop twice
+        in a row we raise an error."""
+        input_str = (
+            'route 1 stops 1 1 2 buses 3 capacity 10',
+            'road 1 2 0.3',
+            'road 2 1 0.5',
+            'road 1 1 0.4',
+            'board 0.5',
+            'disembarks 0.6',
+            'departs 0.5',
+            'new passengers 5',
             'stop time 80',
+            'ignore warnings'
         )
+        network, rates, params, experiments = parse_lines(input_str, 'test')
         with self.assertRaises(InputError):
-            parse_lines(input_str, 'test')
-
-    def test_stop_time_zero_is_error(self):
-        """Verifies that stop time cannot be zero."""
-        input_str = (
-            'route 1 stops 1 2 buses 3 capacity 10',
-            'road 1 2 0.3',
-            'road 2 1 0.2',
-            'board 0.4',
-            'disembarks 0.5',
-            'departs 0.9',
-            'new passengers 3',
-            'stop time 0',
-        )
-        with self.assertRaises(InputError):
-            parse_lines(input_str, 'test')
-
+            network.validate(rates, params)
 
 class TestParserValidation(unittest.TestCase):
     def setUp(self):
@@ -262,6 +272,82 @@ class TestParserValidation(unittest.TestCase):
             parse_lines(input_str, 'test')
         except InputError:
             self.fail()
+
+    def test_rate_zero_is_error(self):
+        """Verifies that if a rate is 0 we raise an error."""
+        input_str = (
+            'route 1 stops 1 2 buses 3 capacity 10',
+            'road 1 2 0.3',
+            'road 2 1 0.2',
+            'board 0.4',
+            'disembarks 0.5',
+            'departs 0',
+            'new passengers 3',
+            'stop time 80',
+        )
+        with self.assertRaises(InputError):
+            parse_lines(input_str, 'test')
+
+    def test_stop_time_zero_is_error(self):
+        """Verifies that stop time cannot be zero."""
+        input_str = (
+            'route 1 stops 1 2 buses 3 capacity 10',
+            'road 1 2 0.3',
+            'road 2 1 0.2',
+            'board 0.4',
+            'disembarks 0.5',
+            'departs 0.9',
+            'new passengers 3',
+            'stop time 0',
+        )
+        with self.assertRaises(InputError):
+            parse_lines(input_str, 'test')
+
+    def test_capacity_zero_is_error(self):
+        """Verifies that buses cannot have 0 capacity."""
+        input_str = (
+            'route 1 stops 1 2 buses 3 capacity 0',
+            'road 1 2 0.3',
+            'road 2 1 0.2',
+            'board 0.4',
+            'disembarks 0.5',
+            'departs 0.9',
+            'new passengers 3',
+            'stop time 10',
+        )
+        with self.assertRaises(InputError):
+            parse_lines(input_str, 'test')
+
+    def test_capacity_zero_is_error(self):
+        """Verifies that routes cannot have 0 buses."""
+        input_str = (
+            'route 1 stops 1 2 buses 0 capacity 10',
+            'road 1 2 0.3',
+            'road 2 1 0.2',
+            'board 0.4',
+            'disembarks 0.5',
+            'departs 0.9',
+            'new passengers 3',
+            'stop time 10',
+        )
+        with self.assertRaises(InputError):
+            parse_lines(input_str, 'test')
+
+    def test_road_to_itself_is_warn(self):
+        """Verifies that a road rate with same stops raises a warning."""
+        input_str = (
+            'route 1 stops 1 2 buses 3 capacity 10',
+            'road 1 2 0.3',
+            'road 2 1 0.7',
+            'road 1 1 0.2',
+            'board 0.4',
+            'disembarks 0.5',
+            'departs 0.9',
+            'new passengers 3',
+            'stop time 10',
+        )
+        with self.assertRaises(InputWarning):
+            parse_lines(input_str, 'test')
 
 
 if __name__ == '__main__':
