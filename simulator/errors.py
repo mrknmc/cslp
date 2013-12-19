a24c1099ef6785b9aa276ff7bcbedc29cf1e39b1
Mark Nemec <mrknmc@me.com>
2013-12-19 02:02:41 +0100

    added a parent class for exceptions so we can catch them later

--- simulator/errors.py
+++ simulator/errors.py
@@ -1,10 +1,14 @@
 
-class InputError(Exception):
+class SimulationException(Exception):
+    """Common base-class so can catch both of the exceptions below."""
+    pass
+
+class InputError(SimulationException):
     """This exception is raised when the input contains an error."""
     pass
 
 
-class InputWarning(Exception):
+class InputWarning(SimulationException):
     """This exception is raised when the input contains something
     that requires a warning."""
     pass
